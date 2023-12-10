d856537d9d26589c19dfef9bab2ccb881d966021
Akkii88 <152087607+Akkii88@users.noreply.github.com>
2023-12-10 13:42:08 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,234 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>about us</h3>
+   <p> <a href="home.php">home</a> / about </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/about-img22.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>At <b>Bookmart</b>, we believe that everyone should have access to the books they love. That's why we offer a wide selection of books at competitive prices, with fast shipping and excellent customer service.</p>
+         <p>We're passionate about helping our customers find the perfect books for their needs, whether they're looking for the latest bestseller, a rare out-of-print title, or something in between.</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="title">client's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/pic-11.png" alt="">
+         <p> Excellent customer service. Fast shipping. Will buy again!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Xavier</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-22.png" alt="">
+         <p>Great selection of books at competitive prices. Highly recommend!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Pulkit arora</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-33.png" alt="">
+         <p>Found the rare out-of-print book I was looking for. Thanks!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Tannu</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-44.png" alt="">
+         <p>Easy to use website and quick checkout.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Rahul</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-55.png" alt="">
+         <p>Books arrived in perfect condition. Very happy with my purchase!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Bhupendra jogi</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-66.png" alt="">
+         <p>Regular customer of this book selling website.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Shivi</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="authors">
+
+   <h1 class="title">greate authors</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/author-11.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Arundhati roy</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-22.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Stephen King</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-33.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Kiran desai</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-44.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john grisham</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-55.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>John Ronald Reuel Tolkien</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-66.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Shashi Tharoor</h3>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,40 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91 8684849586 </p>
+         <p> <i class="fas fa-phone"></i> +91 9416704422 </p>
+         <p> <i class="fas fa-envelope"></i> bookmartofficial@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> rewari, india - 123401 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="https://www.twitter.com/"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href=""> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="https://www.linkedin.com/login"> <i class="fab fa-linkedin"></i> Linkedin </a>
+      </div>
+
+   </div>
+
+
+</section>
